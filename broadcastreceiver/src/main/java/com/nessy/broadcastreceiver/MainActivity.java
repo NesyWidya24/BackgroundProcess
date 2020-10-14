50dbc097ca28d378a9d1d926569e97678ef8f918
kagu244 <nessywidya@gmail.com>
2020-10-14 22:29:17 +0700

    AddBroadcastReceiver

--- broadcastreceiver/src/main/java/com/nessy/broadcastreceiver/MainActivity.java
+++ broadcastreceiver/src/main/java/com/nessy/broadcastreceiver/MainActivity.java
@@ -1,10 +1,14 @@
 package com.nessy.broadcastreceiver;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     Button btnCheck;
@@ -20,9 +24,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View view) {
-        if (view.getId() == R.id.btn_perm){
-            
+        if (view.getId() == R.id.btn_perm) {
+            PermissionManager.check(this, Manifest.permission.RECEIVE_SMS, SMS_REQUEST_CODE);
         }
+    }
 
+    final int SMS_REQUEST_CODE = 101;
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == SMS_REQUEST_CODE) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Sms receiver permission accepted", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Sms receiver permission declined", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
--- broadcastreceiver/src/main/java/com/nessy/broadcastreceiver/SmsReceiver.java
+++ broadcastreceiver/src/main/java/com/nessy/broadcastreceiver/SmsReceiver.java
@@ -6,23 +6,42 @@ import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.telephony.SmsMessage;
+import android.util.Log;
 
 public class SmsReceiver extends BroadcastReceiver {
     final String TAG = SmsReceiver.class.getSimpleName();
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        throw new UnsupportedOperationException("Not yet implemented");
+        final Bundle bundle = intent.getExtras();
+        try {
+            if (bundle != null){
+                final Object[] pdusObj = (Object[]) bundle.get("puds");
+                if (pdusObj != null){
+                    for (Object aPdusObj : pdusObj){
+                        SmsMessage currentMessage = getIncomingMessage(aPdusObj, bundle);
+                        String senderNum = currentMessage.getDisplayOriginatingAddress();
+                        String msg = currentMessage.getDisplayMessageBody();
+                        Log.d(TAG, "senderNum: "+ senderNum + "; message: "+msg);
+                        Intent showSmsIntent = new Intent(context,SmsReceiverActivity.class);
+                        showSmsIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                        showSmsIntent.putExtra(SmsReceiverActivity.EXTRA_SMS_NO, senderNum);
+                        showSmsIntent.putExtra(SmsReceiverActivity.EXTRA_SMS_MSG, msg);
+                        context.startActivity(showSmsIntent);
+                    }
+                }else {
+                    Log.d(TAG, "onReceiver: SMS is null");
+                }
+            }
+        }catch (Exception e){
+            Log.d(TAG, "Exception smsReceiver" + e);
+        }
     }
 
     private SmsMessage getIncomingMessage(Object aObject, Bundle bundle){
         SmsMessage currentSMS;
-        if (Build.VERSION.SDK_INT >=23){
-            String format = bundle.getString("format");
-            currentSMS = SmsMessage.createFromPdu((byte[]) aObject, format);
-        }else {
-            currentSMS = SmsMessage.createFromPdu((byte[]) aObject);
-        }
+        String format = bundle.getString("format");
+        currentSMS = SmsMessage.createFromPdu((byte[]) aObject, format);
         return currentSMS;
     }
 }
