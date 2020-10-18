d875dbd25cf9e0fdb5393444aa4a6ffeab24cc2c
kagu244 <nessywidya@gmail.com>
2020-10-18 21:01:16 +0700

    AddBroadcastReceiver

--- broadcastreceiver/src/main/java/com/nessy/broadcastreceiver/MainActivity.java
+++ broadcastreceiver/src/main/java/com/nessy/broadcastreceiver/MainActivity.java
@@ -4,14 +4,22 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.Manifest;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     Button btnCheck;
+    Button btnDownload;
+    public static final String ACTION_DOWNLOAD_STATUS = "download_status";
+    private BroadcastReceiver downloadReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,12 +28,35 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         btnCheck = findViewById(R.id.btn_perm);
         btnCheck.setOnClickListener(this);
+        btnDownload = findViewById(R.id.btn_download);
+        btnDownload.setOnClickListener(this);
+
+        downloadReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                Log.d(DownloadService.TAG, "Download Done");
+                Toast.makeText(context, "Download Done", Toast.LENGTH_SHORT).show();
+            }
+        };
+        IntentFilter downloadIntentFilter = new IntentFilter(ACTION_DOWNLOAD_STATUS);
+        registerReceiver(downloadReceiver, downloadIntentFilter);
     }
 
     @Override
     public void onClick(View view) {
         if (view.getId() == R.id.btn_perm) {
             PermissionManager.check(this, Manifest.permission.RECEIVE_SMS, SMS_REQUEST_CODE);
+        }else if (view.getId() == R.id.btn_download){
+            Intent downloadServiceIntent = new Intent(this, DownloadService.class);
+            startService(downloadServiceIntent);
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (downloadReceiver != null){
+            unregisterReceiver(downloadReceiver);
         }
     }
 
